bc67f3d5fd8ee50efe8787046b899c17f2ce7e79
Tom Evans <tevans@uk.ibm.com>
2017-03-14 11:49:42 +0000

    Fix apis download and add an integration test

--- liberty-starter-application/src/main/java/com/ibm/liberty/starter/ServiceConnector.java
+++ liberty-starter-application/src/main/java/com/ibm/liberty/starter/ServiceConnector.java
@@ -32,13 +32,13 @@ import com.ibm.liberty.starter.api.v1.model.provider.Sample;
 import com.ibm.liberty.starter.api.v1.model.registration.Service;
 
 public class ServiceConnector {
-    
+
     private static final Logger log = Logger.getLogger(ServiceConnector.class.getName());
-    
+
     private final Services services;
     private String serverHostPort;
     private final String internalServerHostPort;
-    
+
     public ServiceConnector(URI uri) {
         String scheme = uri.getScheme();
         String authority = uri.getAuthority();
@@ -52,24 +52,24 @@ public class ServiceConnector {
         internalServerHostPort = internalHostPort;
         services = parseServicesJson();
     }
-    
+
     public Services parseServicesJson() {
         log.fine("Parsing services json file. INTERNAL_SERVER_HOST_PORT=" + internalServerHostPort);
-        Services services = getObjectFromEndpoint(Services.class, 
-                internalServerHostPort + "/start/api/v1/services", 
+        Services services = getObjectFromEndpoint(Services.class,
+                internalServerHostPort + "/start/api/v1/services",
                                                   MediaType.APPLICATION_JSON_TYPE);
         log.fine("Setting SERVICES object to " + services.getServices());
         return services;
     }
-    
+
     public Services getServices() {
         return services;
     }
-    
+
     public String getServerHostPort() {
         return serverHostPort;
     }
-    
+
     // Returns the service object associated with the given id
     public Service getServiceObjectFromId(String id) {
         log.fine("Return service object for " + id);
@@ -81,13 +81,13 @@ public class ServiceConnector {
         }
         return service;
     }
-    
+
     public Provider getProvider(Service service) {
         String url = urlConstructor("/api/v1/provider", service);
         Provider provider = getObjectFromEndpoint(Provider.class, url, MediaType.APPLICATION_JSON_TYPE);
         return provider;
     }
-    
+
     public String processUploadedFiles(Service service, String uploadDirectory) {
         log.finer("service=" + service.getId() + " : uploadDirectory=" + uploadDirectory);
         String url = urlConstructor("/api/v1/provider/uploads/process?path=" + uploadDirectory, service);
@@ -95,7 +95,7 @@ public class ServiceConnector {
         log.fine("Response of processing uploaded files from " + uploadDirectory + " : " + response);
         return response;
     }
-    
+
     public String prepareDynamicPackages(Service service, String techWorkspaceDir, String options, String[] techs) {
         log.finer("service=" + service.getId() + " : options=" + options + " : techWorkspaceDir=" + techWorkspaceDir + " : techs=" + techs);
         String optionsParam = (options != null && !options.trim().isEmpty()) ? ("&options=" + options) : "";
@@ -106,12 +106,12 @@ public class ServiceConnector {
             log.fine("Response of preparing dynamic packages from " + techWorkspaceDir + " : " + response);
             return response;
         } catch(javax.ws.rs.NotFoundException e){
-            // The service doesn't offer this endpoint, so the exception can be ignored. 
+            // The service doesn't offer this endpoint, so the exception can be ignored.
             log.finest("Ignore expected exception : The service doesn't offer endpoint " + url + " : " + e);
         }
         return "";
     }
-    
+
     public String getFeaturesToInstall(Service service) {
         log.finer("service=" + service.getId());
         String url = urlConstructor("/api/v1/provider/features/install", service);
@@ -120,34 +120,45 @@ public class ServiceConnector {
             log.fine("Features to install for " + service.getId() + " : " + response);
             return response;
         } catch(javax.ws.rs.NotFoundException e){
-            // The service doesn't offer this endpoint, so the exception can be ignored. 
+            // The service doesn't offer this endpoint, so the exception can be ignored.
             log.finest("Ignore expected exception : The service doesn't offer endpoint " + url + " : " + e);
             }
         return "";
     }
-    
+
     public Sample getSample(Service service) {
         String url = urlConstructor("/api/v1/provider/samples", service);
         Sample sample = getObjectFromEndpoint(Sample.class, url, MediaType.APPLICATION_JSON_TYPE);
         return sample;
     }
-    
+
     public InputStream getResourceAsInputStream(String url) {
         return getObjectFromEndpoint(InputStream.class, url, MediaType.WILDCARD_TYPE);
     }
-    
+
     public InputStream getArtifactAsInputStream(Service service, String extension) {
         extension = extension.startsWith("/") ? extension.substring(1) : extension;
         String url = urlConstructor("/artifacts/" + extension, service);
         return getObjectFromEndpoint(InputStream.class, url, MediaType.WILDCARD_TYPE);
     }
-    
+
+    public InputStream getArtifactAsInputStream(String endpoint, String extension) {
+        extension = extension.startsWith("/") ? extension.substring(1) : extension;
+        String url = urlConstructor("/artifacts/" + extension, endpoint);
+        return getObjectFromEndpoint(InputStream.class, url, MediaType.WILDCARD_TYPE);
+    }
+
     private String urlConstructor(String extension, Service service) {
-        log.fine("Constructing url:" + internalServerHostPort + "+" + service.getEndpoint() + "+" + extension);
-        String url = internalServerHostPort + service.getEndpoint() + extension;
+        String url = urlConstructor(extension, service.getEndpoint());
         return url;
     }
-    
+
+    private String urlConstructor(String extension, String endpoint) {
+        log.fine("Constructing url:" + internalServerHostPort + "+" + endpoint + "+" + extension);
+        String url = internalServerHostPort + endpoint + extension;
+        return url;
+    }
+
     public < E > E getObjectFromEndpoint(Class<E> klass, String url, MediaType mediaType) {
         System.out.println("Getting object from url " + url);
         Client client = ClientBuilder.newClient();
@@ -157,7 +168,7 @@ public class ServiceConnector {
         client.close();
         return object;
     }
-    
+
     public Response getResponseFromEndpoint(String url, MediaType mediaType) {
         System.out.println("Getting object from url " + url);
         Client client = ClientBuilder.newClient();
